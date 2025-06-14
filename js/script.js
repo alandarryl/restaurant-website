44eb33044c71fae95cc63a61bbf924b501edaf1a
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-06-14 12:01:15 +0200

    home section complete

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
