2cb18093ae0f619936aa008b099698fb208ade22
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-06-16 14:24:05 +0200

    food part ongoing

--- js/script.js
+++ js/script.js
@@ -19,3 +19,29 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper = new Swiper(".food-slider", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides:true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    }
+});
