a043fc5b600de537eed2fda572b32afe4950c53d
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-06-18 17:57:50 +0200

    completed blogs section

--- js/script.js
+++ js/script.js
@@ -86,5 +86,32 @@ var swiper = new Swiper(".menu-slider", {
 });
 
 
+var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides:true,
+    autoHeight: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    }
+});
+
 
 
